a378f4f3b92390cae5d0032681b8bc84ff26682d
a.d.devnandan <a.d.devnandan@gmail.com>
2026-03-31 19:06:15 +0530

    models added

--- apps/api/config/settings.py
+++ apps/api/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+APP_MODE = os.getenv("APP_MODE", "self_hosted")
 
 # Application definition
 
@@ -73,9 +75,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "owldb",
+        "USER": "postgres",
+        "PASSWORD": "postgres123",
+        "HOST": "localhost",
+        "PORT": 5432,
     }
 }
 
--- apps/api/monitors/models.py
+++ apps/api/monitors/models.py
@@ -1,3 +1,15 @@
+# monitors/models.py
+
 from django.db import models
 
-# Create your models here.
+class Monitor(models.Model):
+    url = models.URLField()
+    name = models.CharField(max_length=255)
+    interval = models.IntegerField(default=60)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Check(models.Model):
+    monitor = models.ForeignKey(Monitor, on_delete=models.CASCADE)
+    status = models.BooleanField()
+    response_time = models.FloatField()
+    checked_at = models.DateTimeField(auto_now_add=True)
